10eecc4485b0f030af74a004ba7a1271ee15c439
jorbuedo_nexum <jordi.bueno@nexum.com>
2020-11-05 12:43:14 +0100

    set defaultValue in cms

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,4 +1,33 @@
+import React from 'react'
+
+type DropdownOption = {
+  id: string
+  label: string
+}
+
+type DropdownParams = {
+  defaultValue: string
+  options: [DropdownOption]
+}
+
 export function Dropdown({ sdk, initialData }: { sdk: any; initialData: any }) {
-  console.log({ sdk, initialData })
-  return <pre>Dropdown</pre>
+  const { defaultValue, options } = sdk.params.instance as DropdownParams
+
+  React.useEffect(() => {
+    sdk.field.setValue(defaultValue)
+  }, [sdk])
+
+  const handleChange = (ev: React.ChangeEvent<HTMLSelectElement>) => {
+    sdk.field.setValue(ev.target.value)
+  }
+
+  return (
+    <select value={initialData || defaultValue} onChange={handleChange}>
+      {options.map(({ id, label }) => (
+        <option key={id} value={id}>
+          {label}
+        </option>
+      ))}
+    </select>
+  )
 }
